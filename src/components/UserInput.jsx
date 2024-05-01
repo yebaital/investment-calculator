a681a8c4e1445a73fe65c40606e05ba20dfe6282
yebaital <yousef.baitalmal@gmail.com>
2024-05-01 16:23:11 +0300

    Add Header and UserInput components to App
    Two new components, Header and UserInput, have been created and incorporated into the main App component. The UserInput component manages state for user inputs including expected return and investment values. Header component displays a logo and title for the application.

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,75 @@
+import {useState} from "react";
+
+export default function UserInput() {
+
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            }
+        });
+    }
+
+    return <section id="user-input">
+        <div className="input-group">
+            <p>
+                <label>Initial Investment</label>
+                <input
+                    type="number"
+                    required
+                    onChange={
+                        (event) =>
+                            handleChange('initialInvestment', event.target.value)
+                    }
+                    value={userInput.initialInvestment}
+                />
+            </p>
+            <p>
+                <label>Annual Investment</label>
+                <input
+                    type="number"
+                    required
+                    onChange={
+                        (event) =>
+                            handleChange('annualInvestment', event.target.value)
+                    }
+                    value={userInput.annualInvestment}
+                />
+            </p>
+        </div>
+        <div className="input-group">
+            <p>
+                <label>Expected Return</label>
+                <input
+                    type="number"
+                    required
+                    onChange={
+                        (event) =>
+                            handleChange('expectedReturn', event.target.value)
+                    }
+                    value={userInput.expectedReturn}
+                />
+            </p>
+            <p>
+                <label>Duration</label>
+                <input
+                    type="number"
+                    required
+                    onChange={
+                        (event) =>
+                            handleChange('duration', event.target.value)
+                    }
+                    value={userInput.duration}
+                />
+            </p>
+        </div>
+    </section>
+}
